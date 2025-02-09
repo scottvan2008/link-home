7df20fe8e27005932202e48a69f893c1751848a4
Scott Yinan Fan <scottvan2008@gmail.com>
2025-02-10 02:48:28 +0800

    17 admin middleware

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -3,16 +3,14 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
 export default function Login() {
     return (
+        <Card>
+            <CardHeader>
+                <CardTitle className="text-3xl font-bold">Login</CardTitle>
+            </CardHeader>
 
-            <Card>
-                <CardHeader>
-                    <CardTitle className="text-3xl font-bold">Login</CardTitle>
-                </CardHeader>
-
-                <CardContent>
-                    <ContinueWithGoogleButton />
-                </CardContent>
-            </Card>
-
+            <CardContent>
+                <ContinueWithGoogleButton />
+            </CardContent>
+        </Card>
     );
 }
---
+++ app/admin-dashboard/page.tsx
@@ -0,0 +1,5 @@
+export default function AdminDashboard(){
+    return (
+        <div>admin dashboard</div>
+    )
+} 
