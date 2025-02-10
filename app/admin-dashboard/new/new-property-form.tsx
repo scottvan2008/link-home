58a3eb70684f2d1e3e778d58ea05f85b9452657b
Scott Yinan Fan <scottvan2008@gmail.com>
2025-02-10 08:53:45 +0800

    22. Create Property

--- app/admin-dashboard/new/new-property-form.tsx
+++ app/admin-dashboard/new/new-property-form.tsx
@@ -1,13 +1,24 @@
 "use client";
 import PropertyForm from "@/components/property-form";
 import { propertyDataSchema } from "@/validation/propertySchema";
+import { PlusCircleIcon } from "lucide-react";
 import { z } from "zod";
 
 export default function NewPropertyForm() {
-    const handleSubmit = async (data: z.infer<typeof propertyDataSchema>) => {};
+    const handleSubmit = async (data: z.infer<typeof propertyDataSchema>) => {
+        console.log({ data });
+    };
+
     return (
         <div>
-            <PropertyForm handleSubmit={handleSubmit} />
+            <PropertyForm
+                handleSubmit={handleSubmit}
+                submitButtonLabel={
+                    <>
+                        <PlusCircleIcon /> Create Property
+                    </>
+                }
+            />
         </div>
     );
 }
