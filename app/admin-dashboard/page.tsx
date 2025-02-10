4411e3a2d0e356d94ff09c573f268c4b06c9224a
Scott Yinan Fan <scottvan2008@gmail.com>
2025-02-10 14:16:35 +0800

    25. Done

--- app/admin-dashboard/page.tsx
+++ app/admin-dashboard/page.tsx
@@ -1,9 +1,12 @@
 import { Breadcrumbs } from "@/components/ui/breadcrumb";
 import { Button } from "@/components/ui/button";
+import { getProperties } from "@/data/properties";
 import { PlusCircleIcon } from "lucide-react";
 import Link from "next/link";
 
-export default function AdminDashboard() {
+export default async function AdminDashboard() {
+    const data = await getProperties();
+    console.log({ data });
     return (
         <div>
             <Breadcrumbs
