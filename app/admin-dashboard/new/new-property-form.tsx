fd0038c072ed3d3b610076d6d760897500d171f1
Scott Yinan Fan <scottvan2008@gmail.com>
2025-02-10 08:34:16 +0800

    21. Select Menu

---
+++ app/admin-dashboard/new/new-property-form.tsx
@@ -0,0 +1,13 @@
+"use client";
+import PropertyForm from "@/components/property-form";
+import { propertyDataSchema } from "@/validation/propertySchema";
+import { z } from "zod";
+
+export default function NewPropertyForm() {
+    const handleSubmit = async (data: z.infer<typeof propertyDataSchema>) => {};
+    return (
+        <div>
+            <PropertyForm handleSubmit={handleSubmit} />
+        </div>
+    );
+}
