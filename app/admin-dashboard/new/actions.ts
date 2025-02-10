4399d58ad3fcfc77f2d5ce734945238f2e2210ae
Scott Yinan Fan <scottvan2008@gmail.com>
2025-02-10 12:19:51 +0800

    save to firebase

---
+++ app/admin-dashboard/new/actions.ts
@@ -0,0 +1,44 @@
+"use server";
+
+import { auth, firestore } from "@/firebase/server";
+import { propertyDataSchema } from "@/validation/propertySchema";
+
+export const saveNewProperty = async (data: {
+    address1: string;
+    address2?: string;
+    city: string;
+    postcode: string;
+    description: string;
+    price: number;
+    bedrooms: number;
+    bathrooms: number;
+    status: "for-sale" | "draft" | "withdrawn" | "sold";
+    token: string;
+}) => {
+    const { token, ...propertyData } = data;
+    const verifiedToken = await auth.verifyIdToken(token);
+
+    if (!verifiedToken.admin) {
+        return {
+            error: "You are not an admin",
+        };
+    }
+
+    const validation = propertyDataSchema.safeParse(propertyData);
+    if (!validation.success) {
+        return {
+            error: true,
+            message:
+                validation.error.issues[0]?.message ?? " An error occurred",
+        };
+    }
+    const property = await firestore.collection("properties").add({
+        ...propertyData,
+        created: new Date(),
+        update: new Date(),
+    });
+
+    return {
+        propertyId: property.id,
+    };
+};
--- app/admin-dashboard/page.tsx
+++ app/admin-dashboard/page.tsx
@@ -23,5 +23,3 @@ export default function AdminDashboard() {
         </div>
     );
 }
-
-//uselessdd
